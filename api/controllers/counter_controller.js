ca21aed2f1825a47c829688ee93ba712332bfa83
Yu-Chen Lin <npes87184@gmail.com>
2020-06-26 12:25:03 +0800

    Remove extra tab
    Signed-off-by: Yu-Chen Lin <npes87184@gmail.com>

--- api/controllers/counter_controller.js
+++ api/controllers/counter_controller.js
@@ -1,23 +1,23 @@
 var model = require("../models/counter_model.js")
 
 exports.ListAllField = function(req, res) {
-		model.List(res);
+	model.List(res);
 };
 
 exports.GetField = function(req, res) {
-		model.Get(req.params.field_id, res);
+	model.Get(req.params.field_id, res);
 };
 
 exports.PutField = function(req, res) {
-		var errors = []
+	var errors = []
 
-		if (!req.body.count) {
-				errors.push("No count specified");
-		}
-		if (errors.length) {
-				res.status(400).json({"error":errors.join(",")});
-				return;
-		}
+	if (!req.body.count) {
+		errors.push("No count specified");
+	}
+	if (errors.length) {
+		res.status(400).json({"error":errors.join(",")});
+		return;
+	}
 
-		model.Put(req.params.field_id, req.body.count, res);
+	model.Put(req.params.field_id, req.body.count, res);
 };
--- api/models/counter_model.js
+++ api/models/counter_model.js
@@ -4,61 +4,61 @@ var file = "./counter.db"
 var sqlite3 = require("sqlite3").verbose();
 
 exports.List = function(res) {
-		let db = new sqlite3.Database(file, (err) => {
-				if (err) {
-						console.log(err.message)
-				}
+	let db = new sqlite3.Database(file, (err) => {
+		if (err) {
+			console.log(err.message)
+		}
+	});
+
+	db.serialize(function() {
+		db.run("CREATE TABLE IF NOT EXISTS counter (field TEXT PRIMARY KEY, count INTEGER NOT NULL, timestamp INTEGER NOT NULL)");
+
+		db.all("SELECT * FROM counter", [], (err, rows) => {
+			if (err) {
+				throw err;
+			}
+			res.json({"message":"success", "data":rows})
 		});
+	});
 
-		db.serialize(function() {
-				db.run("CREATE TABLE IF NOT EXISTS counter (field TEXT PRIMARY KEY, count INTEGER NOT NULL, timestamp INTEGER NOT NULL)");
-
-				db.all("SELECT * FROM counter", [], (err, rows) => {
-						if (err) {
-								throw err;
-						}
-						res.json({"message":"success", "data":rows})
-				});
-		});
-
-		db.close();
+	db.close();
 };
 
 exports.Get = function(field_id, res) {
-		let db = new sqlite3.Database(file, (err) => {
-				if (err) {
-						console.log(err.message)
-				}
-		});
+	let db = new sqlite3.Database(file, (err) => {
+		if (err) {
+			console.log(err.message)
+		}
+	});
 
-		db.serialize(function() {
-				db.run("CREATE TABLE IF NOT EXISTS counter (field TEXT PRIMARY KEY, count INTEGER NOT NULL, timestamp INTEGER NOT NULL)");
-				db.get("SELECT * FROM counter WHERE field=" + field_id, [], (err, row) => {
-						if (err) {
-								throw err;
-						}
-						res.json({"message":"success", "data":row})
-				});
+	db.serialize(function() {
+		db.run("CREATE TABLE IF NOT EXISTS counter (field TEXT PRIMARY KEY, count INTEGER NOT NULL, timestamp INTEGER NOT NULL)");
+		db.get("SELECT * FROM counter WHERE field=" + field_id, [], (err, row) => {
+			if (err) {
+				throw err;
+			}
+			res.json({"message":"success", "data":row})
 		});
+	});
 
-		db.close();
+	db.close();
 };
 
 exports.Put = function(field_id, cnt, res) {
-		let db = new sqlite3.Database(file, (err) => {
-				if (err) {
-						console.log(err.message)
-				}
-		});
+	let db = new sqlite3.Database(file, (err) => {
+		if (err) {
+			console.log(err.message)
+		}
+	});
 
-		db.serialize(function() {
-				db.run("CREATE TABLE IF NOT EXISTS counter (field TEXT PRIMARY KEY, count INTEGER NOT NULL, timestamp INTEGER NOT NULL)");
-				var sql = "INSERT OR IGNORE INTO counter (field, count, timestamp) VALUES (" + field_id + ", 0, " + Date.now() + ")";
-				db.run(sql);
-				sql = "UPDATE counter SET count = " + cnt + ", timestamp = " + Date.now() + " WHERE field=" + field_id;
-				db.run(sql)
-				res.json({"message":"success"})
-		});
+	db.serialize(function() {
+		db.run("CREATE TABLE IF NOT EXISTS counter (field TEXT PRIMARY KEY, count INTEGER NOT NULL, timestamp INTEGER NOT NULL)");
+		var sql = "INSERT OR IGNORE INTO counter (field, count, timestamp) VALUES (" + field_id + ", 0, " + Date.now() + ")";
+		db.run(sql);
+		sql = "UPDATE counter SET count = " + cnt + ", timestamp = " + Date.now() + " WHERE field=" + field_id;
+		db.run(sql)
+		res.json({"message":"success"})
+	});
 
-		db.close();
+	db.close();
 };
